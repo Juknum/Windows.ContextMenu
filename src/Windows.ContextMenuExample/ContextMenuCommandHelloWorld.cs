3e53821d119702ac390092a1ad1d7584303eb577
Julien Constant <julienconstant190@gmail.com>
2026-07-03 01:47:59 +0200

    feat: add example csproj

---
+++ src/Windows.ContextMenuExample/ContextMenuCommandHelloWorld.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using Juknum.Windows.ContextMenu;
+using static Vanara.PInvoke.Shell32;
+
+namespace Juknum.Windows.ContextMenuExample;
+
+[ComVisible(true)]
+[ClassInterface(ClassInterfaceType.None)]
+[Guid("E3629D56-0001-0001-0001-000000000001")]
+internal class ContextMenuCommandHelloWorld : ExplorerCommand {
+
+    public override Guid Guid => new("E3629D56-0001-0001-0001-000000000001");
+    public override string Title => "Hello World";
+    
+    public override string? Icon {
+        get {
+            var assembly = Assembly.GetExecutingAssembly();
+            var path     = assembly.Location;
+
+            if (string.IsNullOrEmpty(path)) return null;
+            return $"{path},-0";
+        }
+    }
+
+    public override bool Execute(IShellItemArray psiItemArray, IBindCtx? pbc) {
+        MessageBox.Show("Hello, World!");
+        return true;
+    }
+  
+}
---
+++ src/Windows.ContextMenuExample/ContextMenuExample.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+using Juknum.Windows.ContextMenu;
+using Juknum.Windows.ContextMenu.Utils;
+using static Vanara.PInvoke.Shell32;
+
+namespace Juknum.Windows.ContextMenuExample;
+
+[ComVisible(true)]
+[ClassInterface(ClassInterfaceType.None)]
+[Guid("E3629D56-49C1-41F8-B7EF-0057EE60126C")]
+internal class ContextMenuExample : ExplorerCommandMenu {
+
+    public override Guid Guid => new("E3629D56-49C1-41F8-B7EF-0057EE60126C");
+    public override string Title => "Example Context Menu";
+    public override IExplorerCommand[] Commands => [
+        new ContextMenuCommandHelloWorld(),
+    ];
+
+    #region COM Registration
+    [ComRegisterFunction]
+    public static void Register(Type type) {
+        RegistrationHelper.RegisterCommand(
+            verbName: "ExampleContextMenu",
+            menuLabel: "Example Context Menu",
+            clsid: type.GUID.ToString("B"),
+            allFiles: true,
+            allFolders: true
+        );
+    }
+
+    [ComRegisterFunction]
+    public static void Unregister(Type _) {
+        RegistrationHelper.UnregisterCommand("ExampleContextMenu");
+    }
+    #endregion
+}
