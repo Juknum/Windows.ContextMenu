34cd5b9ae3cb6a16bf30db48c9eb8bf7ea38356c
Julien Constant <julienconstant190@gmail.com>
2026-07-05 00:35:20 +0200

    chore: move file around to match DLL names

--- src/Juknum.Windows.ContextMenu.Example/ContextMenuCommandHelloWorld.cs
+++ src/Juknum.Windows.ContextMenu.Example/ContextMenuCommandHelloWorld.cs
@@ -1,8 +1,7 @@
 ﻿
-using System.Reflection;
+using Juknum.Windows.ContextMenu;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
-using Juknum.Windows.ContextMenu;
 using static Vanara.PInvoke.Shell32;
 
 namespace Juknum.Windows.ContextMenuExample;
@@ -14,20 +13,12 @@ internal class ContextMenuCommandHelloWorld : ExplorerCommand {
 
     public override Guid Guid => new("E3629D56-0001-0001-0001-000000000001");
     public override string Title => "Hello World";
-    
-    public override string? Icon {
-        get {
-            var assembly = Assembly.GetExecutingAssembly();
-            var path     = assembly.Location;
 
-            if (string.IsNullOrEmpty(path)) return null;
-            return $"{path},-0";
-        }
-    }
+    public override string? Icon => "%SystemRoot%\\System32\\imageres.dll,-20";
 
     public override bool Execute(IShellItemArray psiItemArray, IBindCtx? pbc) {
         MessageBox.Show("Hello, World!");
         return true;
     }
-  
+
 }
